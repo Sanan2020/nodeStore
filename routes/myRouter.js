03950951766e585ef37fc715601e25eb1c74151d
SananHatsatool <wollongong1958@gmail.com>
2025-01-02 16:27:00 +0700

    image with multer

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -2,21 +2,35 @@ const express = require('express')
 const Product = require('../models/products')
 const router = express.Router()
 
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./public/image')
+    },
+    filename:function(req,file,cb){
+        cb(null,Date.now()+".jpg")
+    }
+})
+const upload = multer({
+    storage:storage
+})
+
 router.get('/',(req,res)=>{
     res.render('index')
 })
 
-router.post('/insert',(req,res)=>{
+router.post('/insert',upload.single("image"),(req,res)=>{
     let data = new Product({
         name:req.body.name,
         price:req.body.price,
-        image:req.body.image,
+        image:req.file.filename,
         description:req.body.description
     })
-   console.log(data)
    try{
     Product.seveProduct(data)
-    res.send('save data success')
+    console.log(data)
+    // res.send('save data success')
+    res.redirect('/')
    }catch(err){
     console.error('Error saving user:', err);
    }
