69fe7850944a4d91cd1caefd747c414cdb98c208
SananHatsatool <wollongong1958@gmail.com>
2025-01-19 23:45:26 +0700

    cart

--- public/cart.js
+++ public/cart.js
@@ -1,10 +1,21 @@
 function addTest(obj){
-  console.log(obj['_id']);
+  console.log(obj);
+  console.log(obj.name);
+  console.log(obj[0]);
+  console.log(obj['name']);
 
+  aa = JSON.parse(obj);
+  console.log(typeof(aa));
+
+  console.log(aa);
+  console.log(aa.name);
+  // console.log(aa[0].name);
+  // console.log(aa[0]);
+  console.log(aa['name']);
 }
 
 
-function processSwal(){
+function processSwal(label){
   let timerInterval;
 Swal.fire({
   // icon:'success',
@@ -26,26 +37,20 @@ Swal.fire({
   /* Read more about handling dismissals below */
   if (result.dismiss === Swal.DismissReason.timer) {
     console.log("I was closed by the timer");
-    // $('#count').html(cart.length);
+    $('#count').html(label);
   }
 });
 
 }
 
 
-var cart=[];
-function addtocart(value_id){
-  processSwal();
-  // // console.log(value_id);
-
+function addtocart(item){
+  itemToOBJ = JSON.parse(item);
   let cartLocal = JSON.parse(localStorage.getItem('cartLocal')) || []; // อ่านตะกร้าจาก localStorage หรือเป็น array ว่าง
   var pass =true;
-  // console.log(cartLocal[0].count);
-  // console.log(cartLocal[0].id);
-  // console.log(cartLocal.length);
 
   for(let i=0;i<cartLocal.length;i++){
-    if(value_id == cartLocal[i].id){
+    if(itemToOBJ._id == cartLocal[i].id){
       cartLocal[i].count ++;
       pass = false;
     }
@@ -53,33 +58,17 @@ function addtocart(value_id){
 
   if(pass){
   var obj = {
-        id: value_id,
+        id: itemToOBJ._id,
+        name:itemToOBJ.name,
+        description:itemToOBJ.description,
+        price:itemToOBJ.price,
+        image:itemToOBJ.image,
         count: 1,
       };
       cartLocal.push(obj);
     }
-
-  $('#count').html(cartLocal.length);
+  
   localStorage.setItem('cartLocal', JSON.stringify(cartLocal)); // เก็บตะกร้ากลับลง localStorage
+  // $('#count').html(cartLocal.length);
+  processSwal(cartLocal.length);
 }
-
-//----------------------// // 
-//var cart=[];
-//funtion()
-// var pass =true;
-  // for(let i=0;i<cart.length;i++){
-  //   if(value_id == cart[i].id){
-  //     cart[i].count ++;
-  //     pass = false;
-  //   }
-  // }
- 
-  // if(pass){
-  //   var obj = {
-  //     id: value_id,
-  //     count: 1,
-  //   };
-  //   cart.push(obj);
-  // }
-  // // console.log(obj);
-  // console.log(cart);
